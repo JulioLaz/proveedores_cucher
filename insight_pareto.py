eaf79223631f28cf34104aaa902cd4984207b185
JulioLaz <julioalazarte@gmail.com>
2025-07-23 23:31:10 -0300

    analisis pareto

--- insight_pareto.py
+++ insight_pareto.py
@@ -20,8 +20,8 @@ def generar_insight_ventas(ventas):
     pct_top = val_top / total * 100
    #  val_fmt = format_currency(val_top, '$', locale='es_AR', format='#,##0')
    #  val_fmt = f'val_top:.0f'.replace('.', ',')  # Formatear sin símbolo de moneda
-    val_fmt = "$" + f"{val_top:,.0f}"
-    
+    val_fmt = "$" + f"{val_top:.0f}"
+
 
     texto = f"""
     🧠 **Insight:**
